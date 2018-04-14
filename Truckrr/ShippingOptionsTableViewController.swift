4b799ae437ff4798a1ef04fa6f466cfb5119a409
Drew Patel <drew1patel@gmail.com>
2018-04-14 14:06:16 -0700

    test changes

--- Truckrr/ShippingOptionsTableViewController.swift
+++ Truckrr/ShippingOptionsTableViewController.swift
@@ -8,6 +8,17 @@
 
 import UIKit
 
+
+class MasterViewControllerCell: UITableViewCell{
+    @IBOutlet weak var cellContent: UIView!
+    @IBOutlet weak var thumbnail: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var descriptionBox: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var indicator: UIView!
+    
+}
+
 class ShippingOptionsTableViewController: UITableViewController {
 
     override func viewDidLoad() {
@@ -29,23 +40,26 @@ class ShippingOptionsTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 1
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath) as! MasterViewControllerCell
+        cell.title.text
+            = "test"
 
+        
         // Configure the cell...
 
         return cell
     }
-    */
+ 
 
     /*
     // Override to support conditional editing of the table view.
