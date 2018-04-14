8a04f4a342b429b75f5cbf17f151da81c3dd9467
Drew Patel <drew1patel@gmail.com>
2018-04-13 19:16:53 -0700

    added some eureka forms

--- Truckrr/FirstViewController.swift
+++ Truckrr/FirstViewController.swift
@@ -7,11 +7,48 @@
 //
 
 import UIKit
+import Eureka
 
-class FirstViewController: UIViewController {
+class FirstViewController: FormViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        super.viewDidLoad()
+        form +++ Section("General Information")
+            <<< NameRow("Name"){ row in
+                row.title = "Name of Good"
+                row.placeholder = "Lobster"
+            }
+            <<< TextRow("Description"){ row in
+                row.title = "Description"
+                row.placeholder = "It has claws."
+            }
+            <<< DateRow(){
+                $0.title = "Arrive by Date"
+                $0.value = Date()
+            }
+            <<< TextRow("Description"){ row in
+                row.title = "Description"
+                row.placeholder = "It has claws."
+            }
+            
+            +++ Section("Dimensions & Weight")
+            <<< IntRow("Length"){ row in
+                row.title = "Length (in.)"
+                row.placeholder = "10"
+            }
+            <<< IntRow("Width"){ row in
+                row.title = "Width (in.)"
+                row.placeholder = "10"
+            }
+            <<< IntRow("Height"){ row in
+                row.title = "Height (in.)"
+                row.placeholder = "10"
+            }
+            <<< IntRow("Weight"){ row in
+                row.title = "Weight (oz.)"
+                row.placeholder = "16"
+            }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
