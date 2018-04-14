c3fb28719095fef415a7f154df7878fa7bf830ff
Drew Patel <drew1patel@gmail.com>
2018-04-13 18:53:06 -0700

    Initial Commit

---
+++ Truckrr/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Truckrr
+//
+//  Created by Drew Patel on 4/13/18.
+//  Copyright © 2018 Drew Patel. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
